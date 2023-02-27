5fda6c96fbd633a15bd5e0b735302815e79b3ee7
Leonardo Luz <leonardoluz10@hotmail.com>
2023-02-27 13:51:48 -0300

    Refatorando services

--- db/api/services/Services.js
+++ db/api/services/Services.js
@@ -4,29 +4,23 @@ class Services {
     constructor(nomeModel) {
         this.nomeModel = nomeModel
     }
-
     async solicitarDataBase(where = {}) {
         return await database[this.nomeModel].findAll(where)
     }
-
     async solicitarDataBasePorId(where = {}) {
         return await database[this.nomeModel].findAll(where)
     }
-
     async criarDataBase(where = {}) {
         return await database[this.nomeModel].create(where)
     }
-
     async modificarDataBasePorId(updateDados, where = {}) {
         if (await database[this.nomeModel].update(updateDados, where)) {
             return await this.solicitarDataBasePorId(where)
         }
     }
-
     async excluirDataBasePorId(where) {
         return await database[this.nomeModel].destroy(where)
     }
-
 }
 
 module.exports = Services;
