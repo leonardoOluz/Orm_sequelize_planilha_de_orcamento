b0edb27ae97a38fcecdf7814d2cd1250d6eb5af6
Leonardo Luz <leonardoluz10@hotmail.com>
2023-02-28 11:26:12 -0300

    Refatorando despesas

--- db/api/controllers/DespesasController.js
+++ db/api/controllers/DespesasController.js
@@ -1,21 +1,34 @@
 const database = require('../models')
+const { DespesasServices } = require('../services')
+/* Instanciando nova despesas */
+const Despesas = new DespesasServices();
 
 class DespesasControllers {
+    /* Acessando Despesas  */
     static async acessarDespesasDatabase(req, res) {
         try {
-            const despesasExistente = await database.Despesas.findAll()
+            const despesasExistente = await Despesas.solicitarDataBase()
             return res.status(200).json(despesasExistente)
         } catch (error) {
             return res.status(500).json({ messagem: `Erro ${error}` })
         }
     }
+    /* Acessar Despesas por Id */
     static async acessarDespesaPorId(req, res) {
         const { id } = req.params
         try {
-            const despesaPorId = await database.Despesas.findOne({ where: { id: Number(id) } })
-            return res.status(200).json(despesaPorId)
+            const despesaPorId = await Despesas.solicitarDataBasePorId({ where: { id: Number(id) } })
+            if (Object.keys(despesaPorId).length === 0) {
+                throw new Error('Não existe receitas com id informado!')
+            } else {
+                return res.status(200).json(despesaPorId)
+            }
         } catch (error) {
-            return res.status(500).json({ message: `Erro: ${error}` })
+            if (error.message === 'Não existe receitas com id informado!') {
+                return res.status(400).json({ mensagem: `Não existe receitas para o id ${id} solicitado.` })
+            } else {
+                return res.status(500).json({ message: `Erro: ${error}` })
+            }
         }
     }
     static async criarDespesa(req, res) {
@@ -39,12 +52,12 @@ class DespesasControllers {
         }
     }
     static async deletarDespesaPorId(req, res) {
-        const {id} = req.params;
+        const { id } = req.params;
         try {
-            await database.Despesas.destroy({where: {id: Number(id)}})
-            return res.status(201).json({message: `Despesa de id: ${id}, deletada com sucesso!`})
+            await database.Despesas.destroy({ where: { id: Number(id) } })
+            return res.status(201).json({ message: `Despesa de id: ${id}, deletada com sucesso!` })
         } catch (error) {
-            return res.status(500).json({message: `Erro: ${error}`})
+            return res.status(500).json({ message: `Erro: ${error}` })
         }
     }
 }
